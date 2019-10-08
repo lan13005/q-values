e303e123beb91e49c0ec2eb54bdf930ebcbca47a
lawrence <lng1492@gmail.com>
2019-10-08 13:48:51 -0400

    tracking some new files

---
+++ getInitParams_step2.C
@@ -0,0 +1,45 @@
+#include "main.h"
+
+void getInitParams_step2(){
+    //double binRange[3]={50,0.35,0.8};
+    double binRange[3]={50,0.35,0.8};
+    double fitRange[3]={0.425,0.7};
+    double binRange2[3]={50,0.05,0.25};
+    double fitRange2[2]={0.1,0.17};
+    int dof=numDOFbkg+numDOFsig;
+    double binsize = (binRange[2]-binRange[1])/binRange[0];
+    double binsize2 = (binRange2[2]-binRange2[1])/binRange2[0];
+    //getParamInit(200,0, dof, true, fitRange[0], fitRange[1] ,binsize);
+    //getParamInit(100,100, dof, true, fitRange[0], fitRange[1] ,binsize);
+    //getParamInit(0,200, dof, true, fitRange[0], fitRange[1] ,binsize);
+    //getParamInit(200,0, dof, false, fitRange2[0], fitRange2[1] ,binsize2);
+    //getParamInit(100,100, dof, false, fitRange2[0], fitRange2[1] ,binsize2);
+    //getParamInit(0,200, dof, false, fitRange2[0], fitRange2[1] ,binsize2);
+    getParamInit paramInit = getParamInit ( 200, 6, 0.425, 0.7, binsize, 0.1, 0.17, binsize2);
+    paramInit.loadData();
+    std::vector<double> par0eta = {paramInit.getEta_par0(), paramInit.getEta_par0()/2, 0};
+    std::vector<double> par1eta = {paramInit.getEta_par1(), paramInit.getEta_par1()/2, 0};
+    std::vector<double> par2eta = {0, paramInit.getEta_par2()/2, paramInit.getEta_par2()};
+    std::vector<double> par0pi0 = {paramInit.getPi0_par0(), paramInit.getPi0_par0()/2, 0};
+    std::vector<double> par1pi0 = {paramInit.getPi0_par1(), paramInit.getPi0_par1()/2, 0};
+    std::vector<double> par2pi0 = {0, paramInit.getPi0_par2()/2, paramInit.getPi0_par2()};
+
+    std::vector< std::vector<double>  > allPars;
+    allPars.push_back(par0eta);
+    allPars.push_back(par1eta);
+    allPars.push_back(par2eta);
+    allPars.push_back(par0pi0);
+    allPars.push_back(par1pi0);
+    allPars.push_back(par2pi0);
+    for(int iPar=0; iPar<allPars.size(); ++iPar){
+        for(int iVal=0; iVal<allPars[iPar].size(); ++iVal){
+            cout << allPars[iPar][iVal] << " ";
+        }
+        cout << endl;
+    }
+
+
+    //getParamInit paramInit_pi0 = getParamInit ( countSig,countBkg, dof, false, fitRange2[0], fitRange2[1] ,binsize2);
+    //paramInit_eta
+
+}
